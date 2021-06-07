722c7fb9e3daa9c4532cc91a31da66d12d8e1816
binchoo <jbinchoo@gmail.com>
2021-06-07 16:46:26 +0900

    Learn chapter 5 - Add google oauth config

---
+++ chap5-oauth2-login/src/main/java/com/corndog/springboot/service/posts/PostsService.java
@@ -0,0 +1,56 @@
+package com.corndog.springboot.service.posts;
+
+import com.corndog.springboot.domain.posts.Posts;
+import com.corndog.springboot.domain.posts.PostsRepository;
+import com.corndog.springboot.web.dto.PostsListResponseDto;
+import com.corndog.springboot.web.dto.PostsResponseDto;
+import com.corndog.springboot.web.dto.PostsSaveRequestDto;
+import com.corndog.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor // PostsRepository가 필요한 생성자가 생성됨. 그럼 Spring은 의존성을 주입해 줌.
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없어요. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없어요. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent()); // 해당 id를 갖는 Posts 데이터의 칼럼 값을 대체 대입하는 것으로 update query 된다.
+
+        return id;
+    }
+
+    @Transactional(readOnly = true) // 조회 기능만 있을 때, 속도 개선을 할 수 있다
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //클라이언트에 반환할 DTO 타입으로 변환하여 제공!
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없어요. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+}
---
+++ chap5-oauth2-login/src/main/java/com/corndog/springboot/web/IndexController.java
@@ -0,0 +1,34 @@
+package com.corndog.springboot.web;
+
+import com.corndog.springboot.service.posts.PostsService;
+import com.corndog.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {  // 모델에 posts 값을 설정: posts를 내림차순 정렬한 리스트
+        model.addAttribute("posts", postsService.findAllDesc()); // 주입한 값 사용하여 서버-사이드-렌더링 진행된다.
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+}
